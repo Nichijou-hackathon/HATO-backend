8f03a59f735d028949fb07d9457f6f2ce8c916a5
TotPro <soshi.kaiga@gmail.com>
2025-06-20 00:00:08 +0900

    Reorganizing file structure

---
+++ gemini_service.py
@@ -0,0 +1,28 @@
+from google import genai
+import pydantic
+
+class mood(pydantic.BaseModel):
+    emotion: str
+    color: str
+
+import os
+from dotenv import load_dotenv
+def gemini_prompt(s: str):
+    load_dotenv()
+    key = os.getenv("GEMINI_API_KEY")
+    client = genai.Client(api_key=key)
+    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"{s} に当てはまる感情を [Joy , Sadness , Anger , Surprise , Fear , Anticipation , Relief , Desapair , Curiousity , Achievement , Confusion , Eccitement] のリストの中から1つだけ選択してください. また{s} に当てはまる色をカラーコードで選択してください. 出力はjson形式でお願いします.",
+        config={
+            "response_mime_type": "application/json",
+            "response_schema": mood,
+        },
+    )
+    
+    print(r := response.text)
+    return r
+
+# test
+# gemini_prompt("米津玄師/Lemon")
--- server.py
+++ server.py
@@ -1,32 +1,4 @@
-from google import genai
-import pydantic
-
-class mood(pydantic.BaseModel):
-    emotion: str
-    color: str
-
-import os
-from dotenv import load_dotenv
-def gemini_prompt(s: str):
-    load_dotenv()
-    key = os.getenv("GEMINI_API_KEY")
-    client = genai.Client(api_key=key)
-    
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=f"{s} に当てはまる感情を [Joy , Sadness , Anger , Surprise , Fear , Anticipation , Relief , Desapair , Curiousity , Achievement , Confusion , Eccitement] のリストの中から1つだけ選択してください. また{s} に当てはまる色をカラーコードで選択してください. 出力はjson形式でお願いします.",
-        config={
-            "response_mime_type": "application/json",
-            "response_schema": mood,
-        },
-    )
-    
-    print(r := response.text)
-    return r
-
-# test
-# gemini_prompt("米津玄師/Lemon")
-
+from gemini_service import gemini_prompt
 from flask import Flask, request, jsonify
 import json
 app = Flask(__name__)
@@ -43,4 +15,3 @@ def process_data():
 
 if __name__ == "__main__":
     app.run(port=5000)
-

94837fe5f5df5cce0110ece3bfabf83e3d35e2d3
TotPro <soshi.kaiga@gmail.com>
2025-06-20 18:28:43 +0900

    fix: json parse part at server.py

--- server.py
+++ server.py
@@ -1,14 +1,12 @@
 from gemini_service import gemini_prompt
 from flask import Flask, request, jsonify
-import json
 app = Flask(__name__)
 
 @app.route("/process", methods=["POST"])
 def process_data():
     data = request.get_json()
-    data_dict = json.loads(data)
-    music_name = data_dict["title"]
-    artist_name = data_dict["artist"]
+    music_name = data["title"]
+    artist_name = data["artist"]
     s = music_name + '/' + artist_name
     result = gemini_prompt(s)
     return jsonify({"result": result})
